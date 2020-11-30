4005d66df3c52d2661aa1e3c31aa7d369424be8f
Lxxyx <Lxxyxzj@gmail.com>
2020-11-30 14:55:53 +0800

    ✨ feat(hooks): use overload support type

--- packages/hooks/src/api/useInject.ts
+++ packages/hooks/src/api/useInject.ts
@@ -1,4 +1,6 @@
 import { useContext } from './useContext'
+export function useInject<T>(identifier: new () => T): Promise<T>
+export function useInject<T>(identifier: string): Promise<T>
 
 export async function useInject<T = any>(identifier: any) {
   const { requestContext } = useContext()
